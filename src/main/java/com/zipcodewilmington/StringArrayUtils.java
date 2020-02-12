718da1aa2f65c7f2b11026ddd2b73c26c3aea9b9
Gregory Davis <gdavis@zipcodes-MacBook-Pro.local>
2020-02-12 17:04:19 -0500

    finished first 5 parts, 7 left

--- src/main/java/com/zipcodewilmington/StringArrayUtils.java
+++ src/main/java/com/zipcodewilmington/StringArrayUtils.java
@@ -9,6 +9,8 @@ public class StringArrayUtils {
      * @return first element of specified array
      */ // TODO
     public static String getFirstElement(String[] array) {
+        //code already passes tests
+
         return array[0];
     }
 
@@ -17,6 +19,8 @@ public class StringArrayUtils {
      * @return second element in specified array
      */
     public static String getSecondElement(String[] array) {
+        //code already passes tests
+
         return array[1];
     }
 
@@ -25,7 +29,9 @@ public class StringArrayUtils {
      * @return last element in specified array
      */ // TODO
     public static String getLastElement(String[] array) {
-        return null;
+
+
+        return array[array.length-1];
     }
 
     /**
@@ -33,7 +39,9 @@ public class StringArrayUtils {
      * @return second to last element in specified array
      */ // TODO
     public static String getSecondToLastElement(String[] array) {
-        return null;
+
+
+        return array[array.length-2];
     }
 
     /**
@@ -42,7 +50,14 @@ public class StringArrayUtils {
      * @return true if the array contains the specified `value`
      */ // TODO
     public static boolean contains(String[] array, String value) {
-        return false;
+        boolean containedValue = false;
+        for ( String element : array) {
+            if (element == value) {
+                containedValue = true;
+            }
+
+        }
+        return containedValue;
     }
 
     /**
